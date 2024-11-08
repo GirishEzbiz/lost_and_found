0b17edbdd9e7bcae054768797128c023d9497d63
shubhambangwal <126335293+shubhambangwal@users.noreply.github.com>
2024-11-08 10:42:21 +0530

    updation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
-
+import '../../public/assets/css/main.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
 class MyDocument extends Document {
   render() {
     return (
