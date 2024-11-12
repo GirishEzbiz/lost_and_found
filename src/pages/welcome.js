1bd4a1178247690fd21f33744bd39c3f23207894
shubhambangwal <126335293+shubhambangwal@users.noreply.github.com>
2024-11-12 10:57:53 +0530

    updation

---
+++ src/pages/welcome.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const welcome = () => {
+    return (
+        <div className="container px-3">
+            <div className="pt-4">
+                <h1 className="text-center fw-bold mt-4">Welcome to</h1>
+            </div>
+
+        </div>
+    );
+};
+
+export default welcome;
